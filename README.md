bb17c8d56ec571174cae5b4cb7cfad6b89e94869
thuduyen07 <thuduyen07@gmail.com>
2022-12-08 16:27:45 +0700

    Tutorial: Remote debug

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Reference
 1. [Tutorial: Set value](https://www.jetbrains.com/help/idea/tutorial-set-value.html) - Variable - F2
-2. 
+2. [Tutorial: Remote debug](https://www.jetbrains.com/help/idea/tutorial-remote-debug.html#4ab796af)
+3. [Khôi phục Code bằng Git reflog](https://tuanndl.com/post/khoi_phuc_code_bang_git_reflog)
--- src/main/java/com/thuduyen07/tutorialRemoteDebug/Alphabet.java
+++ src/main/java/com/thuduyen07/tutorialRemoteDebug/Alphabet.java
@@ -1,2 +1,16 @@
-package com.thuduyen07.tutorialRemoteDebug;public class Alphabet {
+package com.thuduyen07.tutorialRemoteDebug;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        System.out.println("Starting");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            try {
+                Thread.sleep(250);  //java.lang
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(c);
+        }
+        System.out.println("Complete");
+    }
 }
