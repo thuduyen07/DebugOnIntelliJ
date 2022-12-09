0890a6ec0dd4bc459248d56d65b0c53baf1cdb64
thuduyen07 <thuduyen07@gmail.com>
2022-12-09 20:19:44 +0700

    Tutorial: Detect concurrency issues - fix race condition

--- src/main/java/com/thuduyen07/tutorialDetectConcurrencyIssues/ConcurrencyTest.java
+++ src/main/java/com/thuduyen07/tutorialDetectConcurrencyIssues/ConcurrencyTest.java
@@ -8,16 +8,18 @@ public class ConcurrencyTest {
     static final List a = Collections.synchronizedList(new ArrayList());
 
     public static void main(String[] args) throws InterruptedException {
-            Thread t = new Thread();
-            t.start();
-            addIfAbsent(17);
-            t.join();
-            System.out.println(a.size());
+        Thread t = new Thread();
+        t.start();
+        addIfAbsent(17);
+        t.join();
+        System.out.println(a.size());
     }
 
     private static void addIfAbsent(int i) {
-        if (!a.contains(i)) {
-            a.add(i);
+        synchronized (a) {
+            if (!a.contains(i)) {
+                a.add(i);
+            }
         }
     }
 
